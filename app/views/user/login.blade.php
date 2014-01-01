0c4577c5b42edd736b5d797298bdc90c79167ea2
jiangrongyong <jiangrongyong@gmail.com>
2014-01-01 15:45:02 +0800

    Add remember me

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -10,20 +10,27 @@
         <h2>{{Lang::get('app.name')}}</h2>
         {{ Form::text("username", Input::get("username"), [
             "placeholder" => Lang::get('login.username'),
-            "class" => "form-control"
+            "class" => "form-control",
+            "required" => "true",
+            "autofocus" => "true",
         ]) }}
         {{ Form::password("password", [
             "placeholder" => Lang::get('login.password'),
-            "class" => "form-control"
+            "class" => "form-control",
+            "required" => "true",
         ]) }}
         @if ($error = $errors->first("password"))
             <div class="error">
                 {{ $error }}
             </div>
         @endif
-        <a href="{{ action('RemindersController@getRemind') }}">Forgot password</a>
+        <label class="checkbox">
+            <input type="checkbox" value="remember-me" />
+            Remember me
+        </label>
         {{ Form::submit("Log in", [
             "class" => "btn btn-lg btn-primary btn-block"
         ]) }}
+        <a href="{{ action('RemindersController@getRemind') }}">Forgot password</a>
     {{ Form::close() }}
 @stop
