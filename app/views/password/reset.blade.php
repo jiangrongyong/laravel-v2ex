898651503e63497d452d54c93ba0e5e5af434816
jiangrongyong <jiangrongyong@gmail.com>
2014-01-01 11:14:37 +0800

    reset view

--- app/views/password/reset.blade.php
+++ app/views/password/reset.blade.php
@@ -1,7 +1,50 @@
-<form action="{{ action('RemindersController@postReset') }}" method="POST">
-    <input type="hidden" name="token" value="{{ $token }}">
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <input type="password" name="password_confirmation">
-    <input type="submit" value="Reset Password">
-</form>
+@extends("layout.guest")
+@section("content")
+    <link rel="stylesheet" href="/css/reset.css">
+    {{ Form::open([
+        "url" => action('RemindersController@postReset'),
+        "autocomplete" => "off",
+        "class" => "form-reset",
+        "role" => "form"
+    ]) }}
+        <h2>{{Lang::get('app.name')}}</h2>
+        @if ($error = $errors->first("token"))
+            <div class="error">
+                {{ $error }}
+            </div>
+        @endif
+        {{ Form::text("email", Input::get("email"), [
+            "placeholder" => "john@example.com",
+            "class" => "form-control"
+        ]) }}
+        @if ($error = $errors->first("email"))
+            <div class="error">
+                {{ $error }}
+            </div>
+        @endif
+        {{ Form::password("password", [
+            "placeholder" => "Password",
+            "class" => "form-control password"
+        ]) }}
+        @if ($error = $errors->first("password"))
+            <div class="error">
+                {{ $error }}
+            </div>
+        @endif
+        {{ Form::password("password_confirmation", [
+            "placeholder" => "Confirm password",
+            "class" => "form-control password_confirmation"
+        ]) }}
+        @if ($error = $errors->first("password_confirmation"))
+            <div class="error">
+                {{ $error }}
+            </div>
+        @endif
+        <div class="error">
+            {{ Session::get('error') }}
+        </div>
+        {{ Form::submit("Reset", [
+            "class" => "btn btn-lg btn-primary btn-block"
+        ]) }}
+    {{ Form::close() }}
+@stop
