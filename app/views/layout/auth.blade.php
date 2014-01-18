04ceda2fbeed806ac2624a319d88127117041fdf
jiangrongyong <jiangrongyong@gmail.com>
2014-01-18 12:58:28 +0800

    Layout css

--- app/views/layout/auth.blade.php
+++ app/views/layout/auth.blade.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/layout/auth.css') }}">
 
-        <script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
-        <script src="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
 
-        <title>{{Lang::get('app.name')}}</title>
-    </head>
-    <body>
-        @include("header")
-            <div class="container">
-                @yield("content")
-            </div>
-        @include("footer")
-    </body>
+    <title>{{Lang::get('app.name')}}</title>
+</head>
+<body>
+@include("header")
+<div class="container">
+    <div class="row row-offcanvas row-offcanvas-right">
+        <div class="col-xs-12 col-sm-9">
+            @yield("content")
+        </div>
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas">
+            @include("sidebar")
+        </div>
+    </div>
+</div>
+@include("footer")
+</body>
 </html>
---
+++ app/views/sidebar.blade.php
@@ -0,0 +1,52 @@
+@section("sidebar")
+<div class="box">
+    <div class="cell">
+        <table cellpadding="0" cellspacing="0" border="0" width="100%">
+            <tbody>
+            <tr>
+                <td width="48" valign="top"><a href="/member/jiangrongyong"><img
+                            src="http://cdn.v2ex.com/avatar/b954/66b8/27658_large.png?m=1382157118" class="avatar"
+                            border="0" align="default" style="max-width: 48px; max-height: 48px;"></a></td>
+                <td width="10" valign="top"></td>
+                <td width="auto" align="left"><span class="bigger"><a
+                            href="/member/jiangrongyong">jiangrongyong</a></span>
+
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="sep10"></div>
+        <table cellpadding="0" cellspacing="0" border="0" width="100%">
+            <tbody>
+            <tr>
+                <td width="33%" align="center"><a href="/my/nodes" class="dark" style="display: block;"><span
+                            class="bigger">7</span>
+
+                        <div class="sep3"></div>
+                        <span class="fade">节点收藏</span></a></td>
+                <td width="34%"
+                    style="border-left: 1px solid rgba(100, 100, 100, 0.4); border-right: 1px solid rgba(100, 100, 100, 0.4);"
+                    align="center"><a href="/my/topics" class="dark" style="display: block;"><span
+                            class="bigger">57</span>
+
+                        <div class="sep3"></div>
+                        <span class="fade">主题收藏</span></a></td>
+                <td width="33%" align="center"><a href="/my/following" class="dark" style="display: block;"><span
+                            class="bigger">8</span>
+
+                        <div class="sep3"></div>
+                        <span class="fade">特别关注</span></a></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="inner">
+        <div class="fr" id="money"><a href="/balance" class="balance_area" style="">1 <img
+                    src="http://cdn.v2ex.com/static/img/gold.png" alt="G" align="absmiddle" border="0"
+                    style="padding-bottom: 2px;"> 8 <img src="http://cdn.v2ex.com/static/img/silver.png" alt="S"
+                                                         align="absmiddle" border="0" style="padding-bottom: 2px;"> 26
+                <img src="http://cdn.v2ex.com/static/img/bronze.png" alt="B" align="absmiddle" border="0"></a></div>
+        <a href="/notifications" class="fade">0 条未读提醒</a></div>
+
+</div>
+@show
