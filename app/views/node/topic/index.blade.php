c19c5e17e0d2e3a6999c3374af475deb3e2b79ed
jiangrongyong <jiangrongyong@gmail.com>
2014-01-18 15:55:31 +0800

    style

--- app/views/node/topic/index.blade.php
+++ app/views/node/topic/index.blade.php
@@ -2,7 +2,7 @@
 @section("content")
 <ul class="list-group">
     @foreach ($topics as $topic)
-    <li class="list-group-item">
+    <li class="list-group-item" style="padding: 8px 15px;">
         <div class="row">
             <div class="col-md-1">
                 <a href="/member/mr7">
@@ -14,7 +14,9 @@
                 <div class="row">
                     <div class="col-md-12">
                         <a href="{{ action('TopicsController@show', array($topic->id)) }}">
-                            <h5>{{ $topic->title }}</h5>
+                            <h5 style="margin-top: 5px;margin-bottom: 5px;color: rgb(119, 128, 135);font-size: 16px;font-weight: normal;">
+                                {{ $topic->title}}
+                            </h5>
                         </a>
                     </div>
                 </div>
@@ -22,13 +24,19 @@
                     <div class="col-md-2">
                         <a href="#" class="badge">{{ $node->name }}</a>
                     </div>
-                    <div class="col-md-3">
-                        <a href="#">{{ $topic->user->username }}</a>
+                    <div class="col-md-2">
+                        <a style="color: rgb(119, 128, 135);font-weight: bold;font-size: 12px;" href="#">
+                            {{ $topic->user->username}}
+                        </a>
+                    </div>
+                    <div class="col-md-2">
+                        <span style="color: rgb(204, 204, 204);font-size: 12px;">2 分钟前</span>
                     </div>
                 </div>
             </div>
             <div class="col-md-1">
-                <a href="{{ action('TopicsController@show', array($topic->id)) }}" class="badge">14</a>
+                <a style="margin-top: 15px;" href="{{ action('TopicsController@show', array($topic->id)) }}"
+                   class="badge">14</a>
             </div>
         </div>
     </li>
