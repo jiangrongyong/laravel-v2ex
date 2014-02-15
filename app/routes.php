4a8d65c38514eca6270d3ab2604f46be4a91ab3b
jiangrongyong <jiangrongyong@gmail.com>
2014-02-15 23:41:11 +0800

    Favorites Follows Nodes

--- app/routes.php
+++ app/routes.php
@@ -6,6 +6,9 @@ Route::resource('nodes', 'NodesController');
 Route::resource('nodes.topics', 'NodesTopicsController');
 Route::resource('members.topics', 'MembersTopicsController');
 Route::resource('members.replies', 'MembersRepliesController');
+Route::resource('members.favorites', 'MembersFavoritesController');
+Route::resource('members.follows', 'MembersFollowsController');
+Route::resource('members.nodes', 'MembersNodesController');
 
 Route::resource('topics', 'TopicsController');
 Route::resource('replies', 'RepliesController');
