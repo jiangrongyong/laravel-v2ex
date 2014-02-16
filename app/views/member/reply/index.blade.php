7f8ee2a9fd7063f82b39c44d057c5265bea1387f
jiangrongyong <jiangrongyong@gmail.com>
2014-02-16 15:56:27 +0800

    Reply View partials

--- app/views/member/reply/index.blade.php
+++ app/views/member/reply/index.blade.php
@@ -7,28 +7,7 @@
     </li>
     <li class="actived">全部回复</li>
 </ol>
-<ul class="list-group">
-    @foreach ($replies as $reply)
-    <li class="list-group-item" style="padding: 8px 15px;">
-        <div class="row">
-            <div class="col-md-10">
-                <a class="" href="{{ action('TopicsController@show', array($reply->topic->id)) }}">
-                    <blockquote>
-                        {{ $reply->topic->title }}
-                    </blockquote>
-                </a>
-            </div>
-            <div class="col-md-2 text-muted small text-right">
-                {{ $reply->getCreatedAtDiffForHumans() }}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                {{ $reply->content }}
-            </div>
-        </div>
-    </li>
-    @endforeach
-</ul>
+
+@include("reply.partials.list")
 <?php echo $replies->links(); ?>
 @stop
--- app/views/member/show.blade.php
+++ app/views/member/show.blade.php
@@ -36,27 +36,7 @@
         <a class="text-muted small" href="{{ action('MembersRepliesController@index', array($member->username)) }}">»更多回复</a>
     </div>
 </div>
-<ul class="list-group">
-    @foreach ($replies as $reply)
-    <li class="list-group-item" style="padding: 8px 15px;">
-        <div class="row">
-            <div class="col-md-10">
-                <a class="" href="{{ action('TopicsController@show', array($reply->topic->id)) }}">
-                    <blockquote>
-                        {{ $reply->topic->title }}
-                    </blockquote>
-                </a>
-            </div>
-            <div class="col-md-2 text-muted small text-right">
-                {{ $reply->getCreatedAtDiffForHumans() }}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                {{ $reply->content }}
-            </div>
-        </div>
-    </li>
-    @endforeach
-</ul>
+
+@include("reply.partials.list")
+
 @stop
