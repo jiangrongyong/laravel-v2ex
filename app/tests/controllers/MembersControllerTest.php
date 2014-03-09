689d74edfafa392586dc93919917a9c74f25b1b7
jiangrongyong <jiangrongyong@gmail.com>
2014-03-09 12:18:22 +0800

    Auth Facade Mock it

--- app/tests/controllers/MembersControllerTest.php
+++ app/tests/controllers/MembersControllerTest.php
@@ -7,6 +7,14 @@ class MembersControllerTest extends TestCase {
     }
 
     public function testIndex() {
+        $user = new User();
+        $user->id = 1;
+        $user->username = 'jiangrongyong';
+        $user->email = 'jiangrongyong@gmail.com';
+
+        // TODO header.blade.php Auth:user() invoke twice.
+        Auth::shouldReceive('user')->times(2)->andReturn($user);
+
         $this->client->request('GET', '/members');
         $this->assertTrue($this->client->getResponse()->isOk());
     }
